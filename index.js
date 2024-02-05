4a9f674292e39c49d2df2e15539d76096969f228
Ahsan Ali Shah <ahsanali.bscssef20@iba-suk.edu.pk>
2024-02-05 21:08:53 +0500

    reached login

--- index.js
+++ index.js
@@ -19,8 +19,6 @@ async function start(){
 
     await page.waitForSelector("#s-lc-new-reservation-form");
 
-    // await selectOption(page, "s-lc-location", "872");
-
     let optionValue = "872";
 
     await page.select('select#s-lc-location', optionValue);
@@ -89,6 +87,31 @@ async function start(){
         console.log(`Failed to select the option with value ${optionValue}.`);
     }
 
+    await page.waitForSelector("#s-lc-go");
+
+    await page.click("#s-lc-go");
+
+    await page.waitForSelector("#eq-time-grid > div.fc-header-toolbar.fc-toolbar.fc-toolbar-ltr > div:nth-child(1) > button.fc-goToDate-button.btn.btn-default.btn-sm");
+
+    await page.click(".fc-goToDate-button")
+
+    await page.waitForSelector("#equip_ > div.datepicker.datepicker-dropdown.dropdown-menu.datepicker-orient-left.datepicker-orient-bottom > div.datepicker-days > table > tbody > tr:nth-child(2) > td:nth-child(4)")
+
+    await page.click("#equip_ > div.datepicker.datepicker-dropdown.dropdown-menu.datepicker-orient-left.datepicker-orient-bottom > div.datepicker-days > table > tbody > tr:nth-child(2) > td:nth-child(4)")
+
+    const desiredTitle = "01:00 Wednesday, February 7, 2024 - Quiet Study Room 2.04: Desk 482 - Available";
+
+    await page.waitForSelector(`a[title="${desiredTitle}"]`);
+
+    await page.click(`a[title="${desiredTitle}"]`);
+
+    await page.waitForSelector('#submit_times', { visible: true });
+
+
+    await page.click("#submit_times");
+    await page.waitForNavigation();
+    await page.waitForNavigation();
+
     const newUrl = page.url();
     const newTitle = await page.title();
 
@@ -96,25 +119,6 @@ async function start(){
 
 
     await browser.close();
-}
-
-async function selectOption(page, id, optionValue){
-
-    await page.select('select#'+id, optionValue);
-  
-    selectedValue = await page.evaluate(({id: id}) => {
-          // Replace 'yourSelectElementId' with the actual ID of your select element
-        const select = document.querySelector('select#'+id);
-        return select.value;
-    });
-      
-        // Verify if the selected value matches the expected value
-    if (selectedValue === optionValue) {
-          console.log(`Option with value ${optionValue} was successfully selected.`);
-    } else {
-        console.log(`Failed to select the option with value ${optionValue}.`);
-    }
-
 }
 
 start();
